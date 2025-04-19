240a927938d2a42a9b631baa308a98faf6d8582b
cintramachado <cintramachado1@gmail.com>
2025-04-19 08:12:48 -0400

    pensando em mudoar os menubox para o dashboard administrativo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,21 @@ export default function Home() {
         <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-700 text-center px-4">
           Este é um sistema de gerenciamento de chamadas, onde você pode gerenciar suas ao Painel Eletrônico de forma eficiente e prática.
         </p>
+      
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
       <Menubox 
         imageSrc="/images/medico.jpeg" 
         title="Cadastro de Médicos" 
         description="Cadastrar, excluir e alterar médicos." 
         link="/desired-link" 
       />
+      <Menubox 
+        imageSrc="/images/medico.jpeg" 
+        title="Cadastro de Médicos" 
+        description="Cadastrar, excluir e alterar médicos." 
+        link="/desired-link" 
+      />
+      </div>
       </main>
       <footer className="bg-gray-800 text-white py-4 text-center">
         <p>&copy; 2023 Chamador Top. Todos os direitos reservados.</p>
